d238adca02afac4975cb30f845503f16462de838
Alexander Scott <xander@cadcoder.com>
2017-05-08 21:48:08 +1000

    Course/Divison PAge

--- themes/toomby/acf/init.php
+++ themes/toomby/acf/init.php
@@ -1,22 +1,23 @@
 <?php
 
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page(array(
-		'page_title' 	=> 'General Settings',
-		'menu_title'	=> 'Settings',
-		'menu_slug' 	=> 'theme-general-settings'
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Contact Details',
-		'menu_title'	=> 'Contact Details',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Social Media Settings',
-		'menu_title'	=> 'Social Media',
-		'parent_slug'	=> 'theme-general-settings',
-	));
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page(array(
+        'page_title' => 'General Settings',
+        'menu_title' => 'Settings',
+        'menu_slug' => 'theme-general-settings'
+    ));
+    acf_add_options_sub_page(array(
+        'page_title' => 'Contact Details',
+        'menu_title' => 'Contact Details',
+        'parent_slug' => 'theme-general-settings',
+    ));
+    acf_add_options_sub_page(array(
+        'page_title' => 'Social Media Settings',
+        'menu_title' => 'Social Media',
+        'parent_slug' => 'theme-general-settings',
+    ));
 }
 
 include_once('contact_details.php');
 include_once('social_media_links.php');
+add_filter('acf_the_content', 'wpautop');
