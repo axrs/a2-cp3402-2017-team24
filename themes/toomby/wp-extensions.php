17b645a4bfc142f013f733732c8ad50478469bc7
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-05-10 09:23:20 +1000

    Added articles post type

--- themes/toomby/wp-extensions.php
+++ themes/toomby/wp-extensions.php
@@ -42,6 +42,17 @@ function create_post_types()
             'has_archive' => true,
         )
     );
+
+    register_post_type('article',
+        array(
+            'labels' => array(
+                'name' => __('Articles'),
+                'singular_name' => __('Article')
+            ),
+            'public' => true,
+            'has_archive' => true,
+        )
+    );
 }
 
 add_action('init', 'create_post_types');
