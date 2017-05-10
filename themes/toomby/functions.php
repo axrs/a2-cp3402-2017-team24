fb1e1e0999fb0b3d5b09546324e34c5407763f31
Dale Sedgman <dale.sedgman@my.jcu.edu.au>
2017-05-10 16:22:29 +1000

    google api fixed and contact now shows custom map

--- themes/toomby/functions.php
+++ themes/toomby/functions.php
@@ -1,20 +1,13 @@
 <?php
 
 include_once('wp-extensions.php');
-function my_acf_settings_dir( $dir ) {
-    $dir = get_stylesheet_directory_uri() . '/plugins/advanced-custom-fields-pro/';
-    return $dir;
-}
-// Google Maps API Key
-// function my_acf_google_map_api( $api ){
-	
-// 	$api['key'] = 'AIzaSyAAFdy6HjA3FwsSAniy2Znro161tBK2t2M';
-	
-// 	return $api;
-	
-// }
 
+function my_acf_google_map_api( $api ){
+        $api['key'] = 'AIzaSyAAFdy6HjA3FwsSAniy2Znro161tBK2t2M';
+        return $api;
+    };
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
 if (!class_exists('Timber')) {
     add_action('admin_notices', function () {
         echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
@@ -24,7 +17,7 @@ if (!class_exists('Timber')) {
     add_filter('template_include', function ($template) {
         return get_stylesheet_directory() . '/static/no-timber.html';
     });
-
+    
     return;
 }
 
@@ -78,6 +71,7 @@ class StarterSite extends TimberSite
         return $twig;
     }
 
+    
 }
 
 new StarterSite();
