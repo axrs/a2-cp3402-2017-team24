a852ecdda3b2136a0b94bfa7a5f6cf39e7528b9f
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-05-13 13:26:02 +1000

    Article card now only shows if there are additional articles to show

--- themes/toomby/single.php
+++ themes/toomby/single.php
@@ -13,8 +13,17 @@ $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
+$related_query = array(
+    'post_type' => $post->post_type,
+    'post__not_in' => array($post->ID),
+    'orderby' => ($post->post_type == 'staff') ? 'rand' : 'desc',
+    'posts_per_page' => ($post->post_type == 'staff') ? 4 : 3
+);
+
+$context['related_posts'] = Timber::get_posts($related_query);
+
+if (post_password_required($post->ID)) {
+    Timber::render('single-password.twig', $context);
 } else {
-	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+    Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
 }
