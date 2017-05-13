bb144a74753e0cd686d7027dc39206d52ad76a57
Alexander Scott <xander@cadcoder.com>
2017-05-13 12:33:42 +1000

    include vendor for dist only

--- themes/toomby/functions.php
+++ themes/toomby/functions.php
@@ -1,6 +1,11 @@
 <?php
 
-include_once('wp-extensions.php');
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    // Include vendor files if they exist (only required for dist)
+    include_once(__DIR__ . '/vendor/autoload.php');
+}
+
+require_once('wp-extensions.php');
 
 if (!class_exists('Timber')) {
     add_action('admin_notices', function () {
